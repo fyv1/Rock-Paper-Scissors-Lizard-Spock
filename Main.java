5fe1ba0d09e9bbad65b4fb954f19aa3f4d802b98
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2017-12-27 17:18:02 +0100

    main game class

--- Main.java
+++ Main.java
@@ -8,8 +8,12 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner s = new Scanner(System.in);
-        Game g = new Game(s.nextInt());
 
+        System.out.println("1 - rock \t 2 - paper \t 3 - scissor \t 4 - lizard \t 5 - spock");
+
+        System.out.println("What's your choice?: ");
+        Game g = new Game(s.nextInt());
+        g.whoWon();
 
     }
 }
