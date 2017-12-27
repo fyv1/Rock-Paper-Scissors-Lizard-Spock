2761c0be6f2aa5870f172cec145624073539947d
Grzegorz Nowak <grzegorz.nowak.formal@gmail.com>
2017-12-27 16:29:14 +0100

    improving mechanism

--- Object.java
+++ Object.java
@@ -1,13 +1,41 @@
 package pl.fyv;
 
+import java.rmi.UnexpectedException;
+
 public enum Object {
 
-    ROCK,
-    PAPER,
-    SCISSOR,
-    LIZARD,
-    SPOCK;
+    ROCK(1),
+    PAPER(2),
+    SCISSOR(3),
+    LIZARD(4),
+    SPOCK(5);
 
+    public boolean beats(Object things) throws UnexpectedException {
+        switch(this) {
+            case ROCK:
+                return things == SCISSOR && things == LIZARD;
+            case PAPER:
+                return things == ROCK && things == SPOCK;
+            case SCISSOR:
+                return things == PAPER && things == LIZARD;
+            case LIZARD:
+                return things == PAPER && things == SPOCK;
+            case SPOCK:
+                return things == ROCK && things == SCISSOR;
+            default:
+                throw new UnexpectedException("Nieznany obiekt");
+        }
+    }
+
+
+    private int number;
+    Object(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
 }
 
 
